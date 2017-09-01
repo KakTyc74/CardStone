cec91a7e966e4fd5e84e0633bcc405e181ad1e6d
KakTyc74 <m9514879273@gmail.com>
2017-09-01 13:21:54 +0500

    show button

--- Showtext.py
+++ Showtext.py
@@ -19,20 +19,24 @@ clock = pygame.time.Clock()
 #rhsbImg = pygame.image.load('prototype.png')
 
 
+def btn(btnx, btny, btnw, btnh, color, text, fontsize):
+    pygame.draw.rect(gameDisplay, color, [btnx, btny, btnw, btnh])
+    message_display(text, btnx+btnw/2, btny+btnh/2, fontsize)
 
 def text_objects(text, font):
     TextSurface = font.render(text, True, red, black)
     return TextSurface, TextSurface.get_rect()
 
-def message_display(text):
-    largeText = pygame.font.Font('freesansbold.ttf', 35)
+def message_display(text,x,y,fontsize):
+    largeText = pygame.font.Font('freesansbold.ttf', fontsize)
     TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2),(display_height/2)) 
+    TextRect.center = (x,y) 
     gameDisplay.blit(TextSurf, TextRect)
 
     
 def choice():
-    message_display("Выбери карту из 4")
+    message_display("Выбери карту из 4", display_width/2, display_height/2,35)
+    btn(display_width/2, display_height/2+100, 50, 50, red, 'card1', 12)
 
 while True:
     for event in pygame.event.get():
@@ -43,8 +47,3 @@ while True:
     choice()
     pygame.display.update()
     clock.tick(60)
-
-
-
-#def game_loop():
-
